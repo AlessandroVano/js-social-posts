0feaa8ae3ec57c2a8a51a4cd811e2ddf93a2e233
AlessandroVano <alessandrovanoli.16@gmail.com>
2021-11-05 16:01:57 +0100

    creazione array di oggetti + creazione funzione, richiamo funzione

--- main.js
+++ main.js
@@ -4,13 +4,115 @@
  */
 
 
-
 // 1. creazione struttura di array di oggetti
 // 2. richiamare il container del post tramite una costante
-// 3. luppiamo per generare il tutto
+// 3. luppiamo per generare il tutto (creazione funzione con ciclo for)
 // 4. generiamo tutti e 5 i post
 
 
 
 
+// 1. creazione struttura di array di oggetti
+ const posts = [
+     {
+        profile: 'https://unsplash.it/300/300?image=15',
+        author: 'Phil Mangione',
+        time: '4 mesi fa',
+        text: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+        image: 'https://unsplash.it/600/300?image=171',
+        numLike: '80',
+    },
+    {
+        profile: 'https://unsplash.it/300/300?image=15',
+        author: 'Phil Mangione',
+        time: '4 mesi fa',
+        text: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+        image: 'https://unsplash.it/600/300?image=171',
+        numLike: '80',
+    },
+    {
+        profile: 'https://unsplash.it/300/300?image=15',
+        author: 'Phil Mangione',
+        time: '4 mesi fa',
+        text: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+        image: 'https://unsplash.it/600/300?image=171',
+        numLike: '80',
+    },
+    {
+        profile: 'https://unsplash.it/300/300?image=15',
+        author: 'Phil Mangione',
+        time: '4 mesi fa',
+        text: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+        image: 'https://unsplash.it/600/300?image=171',
+        numLike: '80',
+    },
+    {
+        profile: 'https://unsplash.it/300/300?image=15',
+        author: 'Phil Mangione',
+        time: '4 mesi fa',
+        text: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+        image: 'https://unsplash.it/600/300?image=171',
+        numLike: '80',
+    },
+    
+   
+     
+ ]
 
+
+ console.table(posts)
+
+
+
+ // 2. richiamare il container del post tramite una costante
+ const postsContainer = document.querySelector('.posts-list');
+
+
+  getFullPosts(posts, postsContainer);
+
+
+
+
+
+ /**************************************************************
+                           FUNZIONI
+  **************************************************************/
+
+function getFullPosts(posts, postsContainer){
+    for(let i = 0; i <posts.length; i++ ) {
+        const numPost = posts[i];
+        postsContainer.innerHtml += `
+        <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${numPost.profile}">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${numPost.author}</div>
+                        <div class="post-meta__time">${numPost.time}</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">${numPost.text}</div>
+            <div class="post__image">
+                <img src="${numPost.image}">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${numPost.numLike}</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>
+        `;
+    }
+}
+ 
