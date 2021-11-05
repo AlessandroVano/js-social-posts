24df52fee99545663eafc695ea0b7b838adad15e
AlessandroVano <alessandrovanoli.16@gmail.com>
2021-11-05 14:26:48 +0100

    initialization repo + processi logici in italiano

---
+++ main.js
@@ -0,0 +1,16 @@
+/**
+ * Ricreiamo un feed social aggiungendo al layout di base fornito il nostro codice javascript in cui:
+- Creiamo il nostro array di 5 oggetti che rappresentano ciascun post. Ogni post dovrà avere le informazioni necessarie per stampare la relativa card: nome autore, foto profilo, data, testo del post, immagine, numero di likes.
+ */
+
+
+
+// 1. creazione struttura di array di oggetti
+// 2. richiamare il container del post tramite una costante
+// 3. luppiamo per generare il tutto
+// 4. generiamo tutti e 5 i post
+
+
+
+
+
